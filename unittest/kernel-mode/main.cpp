538d40290d9507b6a56592cfe6ffad16b3988194
MiroKaku <50670906+MiroKaku@users.noreply.github.com>
2020-05-27 15:58:59 +0800

    add example

--- unittest/kernel-mode/main.cpp
+++ unittest/kernel-mode/main.cpp
@@ -1,6 +1,13 @@
-#include <ntddk.h>
+#define _KERNEL_MODE
+
 #include <wdm.h>
+#include <ntddk.h>
 #include <ucxxrt.h>
+
+#include <string>
+#include <random>
+#include <unordered_map>
+
 
 class Test
 {
@@ -41,6 +48,19 @@ EXTERN_C NTSTATUS DriverMain(PDRIVER_OBJECT aDriverObject, PUNICODE_STRING aRegi
         catch (UNICODE_STRING & e)
         {
             DbgPrintEx(DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL, "Catch Exceptrion: %wZ\n", &e);
+        }
+
+        auto vRand = std::mt19937_64(rand());
+        auto vMap  = std::unordered_map<uint32_t, std::string>();
+        for (auto i = 0u; i < 100; ++i)
+        {
+            vMap[i] = std::to_string(vRand());
+        }
+
+        for (const auto& vItem : vMap)
+        {
+            DbgPrintEx(DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL,
+                "map[%ld] = %s\n", vItem.first, vItem.second.c_str());
         }
 
         aDriverObject->DriverUnload = DriverUnload;
