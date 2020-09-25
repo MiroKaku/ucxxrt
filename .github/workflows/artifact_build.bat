2a62c67cc190e1e9c9e49669d0b9a96170873535
MiroKaku <50670906+MiroKaku@users.noreply.github.com>
2020-09-25 13:44:12 +0800

    [Fix] Actions batch

--- .github/workflows/artifact_build.bat
+++ .github/workflows/artifact_build.bat
@@ -1,7 +1,7 @@
 @echo off
 pushd "%~dp0"
 
-powershell Compress-7Zip "ucxxrt" -ArchiveFileName "ucxxrt.zip" -Format Zip
+powershell Compress-7Zip "..\..\ucxxrt" -ArchiveFileName "..\..\ucxxrt.zip" -Format Zip
 
 :exit
 popd
