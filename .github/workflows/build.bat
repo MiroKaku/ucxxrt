9b7941a2e2244e24199c564a5f8b7b8905a1100e
MiroKaku <50670906+MiroKaku@users.noreply.github.com>
2022-03-29 15:50:40 +0800

    fix ci

--- .github/workflows/build.bat
+++ .github/workflows/build.bat
@@ -5,11 +5,11 @@ if exist bin rd /s /q bin
 if exist lib rd /s /q lib
 if exist ucxxrt rd /s /q ucxxrt
 
-"%PROGRAMFILES(x86)%\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\msbuild.exe" /p:Configuration=Release /p:Platform=x64 ..\..\ucxxrt.sln -t:kernel_mode\ucxxrt
-"%PROGRAMFILES(x86)%\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\msbuild.exe" /p:Configuration=Release /p:Platform=x86 ..\..\ucxxrt.sln -t:kernel_mode\ucxxrt
+"%PROGRAMFILES(x86)%\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\msbuild.exe" /p:Configuration=Release /p:Platform=x64 ..\..\ucxxrt.sln -t:ucxxrt
+"%PROGRAMFILES(x86)%\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\msbuild.exe" /p:Configuration=Release /p:Platform=x86 ..\..\ucxxrt.sln -t:ucxxrt
 
-"%PROGRAMFILES(x86)%\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\msbuild.exe" /p:Configuration=Debug /p:Platform=x64 ..\..\ucxxrt.sln -t:kernel_mode\ucxxrt
-"%PROGRAMFILES(x86)%\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\msbuild.exe" /p:Configuration=Debug /p:Platform=x86 ..\..\ucxxrt.sln -t:kernel_mode\ucxxrt
+"%PROGRAMFILES(x86)%\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\msbuild.exe" /p:Configuration=Debug /p:Platform=x64 ..\..\ucxxrt.sln -t:ucxxrt
+"%PROGRAMFILES(x86)%\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\msbuild.exe" /p:Configuration=Debug /p:Platform=x86 ..\..\ucxxrt.sln -t:ucxxrt
 
 :exit
 popd
