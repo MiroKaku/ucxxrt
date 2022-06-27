2166988aa760d6508439891ef5cc8565855f257d
MiroKaku <50670906+MiroKaku@users.noreply.github.com>
2022-06-27 15:41:42 +0800

    fix(veil): unresolved external symbol 'ExAllocateFromNPagedLookasideList' on WDK:22621

--- src/crt/i386/ehprolog.asm
+++ src/crt/i386/ehprolog.asm
@@ -9,28 +9,28 @@
 ;       up EBP as frame base pointer.
 ;
 ;*******************************************************************************
-	title	ehprolog.asm
-	.386
+    title   ehprolog.asm
+    .386
 
 .model FLAT
 
 ASSUME FS : NOTHING
 
-PUBLIC	__EH_prolog
+PUBLIC  __EH_prolog
 
 .code
 
 __EH_prolog PROC NEAR
-	push	-1			; State index
-	push	eax			; Push address of handler thunk
-	mov	eax, DWORD PTR fs:[0]
-	push	eax			; List link
-	mov	eax, DWORD PTR [esp+12]	; Load return address
-	mov	DWORD PTR fs:[0], esp
-	mov	DWORD PTR [esp+12], ebp	; Save old ebp on the stack
-	lea	ebp, DWORD PTR [esp+12]	; Set ebp to the base of the frame
-	push	eax			; Push return addr on top of the stack
-	ret	0				; JMP [eax] would be bad on P6
+    push    -1                      ; State index
+    push    eax                     ; Push address of handler thunk
+    mov     eax, DWORD PTR fs:[0]
+    push    eax                     ; List link
+    mov     eax, DWORD PTR [esp+12] ; Load return address
+    mov     DWORD PTR fs:[0], esp
+    mov     DWORD PTR [esp+12], ebp ; Save old ebp on the stack
+    lea     ebp, DWORD PTR [esp+12] ; Set ebp to the base of the frame
+    push    eax                     ; Push return addr on top of the stack
+    ret     0                       ; JMP [eax] would be bad on P6
 __EH_prolog ENDP
 
 END
