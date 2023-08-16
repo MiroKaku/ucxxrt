ce8da3003db14aa373091ec2fce61e8cbd0ede7c
MiroKaku <50670906+MiroKaku@users.noreply.github.com>
2023-08-16 11:50:06 +0800

    update: veil

--- src/crt/stl/xrngdev.cpp
+++ src/crt/stl/xrngdev.cpp
@@ -8,12 +8,7 @@
 
 _STD_BEGIN
 _CRTIMP2_PURE unsigned int __CLRCALL_PURE_OR_CDECL _Random_device() { // return a random value
-    unsigned int ans;
-    if (_CSTD rand_s(&ans)) {
-        _Xout_of_range("invalid random_device value");
-    }
-
-    return ans;
+    return rand();
 }
 
 _STD_END
